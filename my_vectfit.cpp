06a3d834a6139d4397d14479b3c221e672a2aed9
Piotr Hajdukiewicz <email@email.com>
2016-02-18 15:16:56 +0100

    Zmiany do all splitting - kompiluje sie

--- my_vectfit.cpp
+++ my_vectfit.cpp
@@ -31,15 +31,225 @@ int sign( double x )
         return 1;
     }
 }
+
+
+void vf_all::operator() ( const blocked_range<int>& r ) const
+{
+    for ( int rr = r.begin(); rr != r.end() ; rr++ )
+    {
+        int N = poles1.n_elem, //rzad rozwiazania
+            Ns = f->n_elem; // liczba probek pomiarowych
+        mat imag_check = zeros<mat>(1, N); //wektor informujacy czy dany biegun jest zespolony
+        cx_vec poles = poles1;
+    
+        for ( int i = 0; i < N; i++ )
+        {
+            if ( imag(poles(i)) > 0 ) 
+            {
+                if ( i == 0 )
+                {
+                    imag_check(i) = 1;
+                }
+                else
+                {
+                    if ( imag_check(i-1) == 0 || imag_check(i-1) == 2 )
+                    {
+                        imag_check(i) = 1; imag_check(i+1) = 2;
+                    }
+                    else
+                    {
+                        imag_check(i) = 2;
+                    }
+                }
+            }
+        }
+    
+        cx_mat A = zeros<cx_mat>(Ns, 2*N+1);
+    
+        // wypelnienie lewej strony macierzy A
+        for ( int m = 0; m < N ; m++ )
+        {
+            if ( imag_check(m) == 0 )
+            {
+                A.col(m) = cx_double(1,0) / ( *s - poles(m));
+            }
+            else if ( imag_check(m) == 1 )
+            {
+                A.col(m) = cx_double(1,0) / ( *s - poles(m)) + cx_double(1,0) / ( *s - conj(poles(m)) );
+                A.col(m+1) = 1.0i / ( *s - poles(m) ) - 1.0i / ( *s - conj(poles(m)) );
+            }
+        }
+    
+        A.col(N) = ones<cx_mat>(1,Ns).st();
+    
+        // wypelnienie prawej strony macierzy A
+    
+        for ( int i = 0; i < N; i++ )
+        {
+            A.col(i+N+1) = - strans(f->operator()(0, span(0, Ns-1))) % A( span(0, Ns-1), i);
+        }
+    
+        // obliczanie x metoda najmniejszych kwadratow Ax=b
+        mat A_real = join_vert( real(A), imag(A) );
+    
+        cx_mat f_lsp = f->st();
+        mat f_lsp_real = join_vert( real(f_lsp), imag(f_lsp) );
+    
+        // dokompozycja QR macierzy A
+        mat Q, R;
+        qr(Q, R, A_real);
+    
+        mat bb = Q.st() * f_lsp_real;
+        bb = bb.rows(N+1, 2*N);
+    
+        mat AA = R( span(N+1, 2*N), span(N+1, 2*N) );
+        mat x = solve(AA, bb);
+        
+        // przy pomocy metody wartosci wlasnych macierzy obliczanie zer funkcji sigma - szukane bieguny
+    
+        cx_mat poles_diag = diagmat(poles);
+        mat b_ones = ones<mat>(N,1);
+        mat x_trans = x.st();
+        //int x_trans_end = x_trans.n_elem - 1;
+        //x_trans = x_trans( 1, span( x_trans_end-N, x_trans_end ) );
+        int m = 0;
+    
+        mat poles_diag_real = zeros<mat>(N, N);
+        for ( int n = 0 ; n < N ; n++ )
+        {
+            if ( m < N )
+            {
+                if ( abs(poles_diag(m,m)) > abs(real(poles_diag(m,m))) ) // sprawdzenie czy otrzymany biegun jest zespolony
+                {
+    			poles_diag_real(m+1,m)=-imag(poles_diag(m,m));
+    			poles_diag_real(m,m+1)=imag(poles_diag(m,m));
+    			poles_diag_real(m,m)=real(poles_diag(m,m));
+    			poles_diag_real(m+1,m+1)=poles_diag_real(m,m);
+    			b_ones(m,1)=2; b_ones(m+1,1)=0;
+    			m=m+1;
+                }
+                else
+                {
+                    poles_diag_real(m,m) = real(poles_diag(m, m));
+                }
+            }
+            m++;
+        }
+    
+        //obliczanie wartosci wlasnych macierzy
+        mat H = poles_diag_real - b_ones * x_trans;
+        //cx_mat Hi = cx_mat(H, zeros<mat>(N,N));
+    	poles = eig_gen(H);
+    
+    //=============================================
+    // obliczanie residuów szukanej funkcji
+    // ============================================
+        // sparwdzenie ktore bieguny sa zespolone
+        imag_check = zeros<mat>(1,N);
+        for ( int i = 0; i < N; i++ )
+        {
+            if ( imag(poles(i)) != 0 ) 
+            {
+                if ( i == 0 )
+                {
+                    imag_check(i) = 1;
+                }
+                else
+                {
+                    if ( imag_check(i-1) == 0 || imag_check(i-1) == 2 )
+                    {
+                        imag_check(i) = 1; imag_check(i+1) = 2;
+                    }
+                    else
+                    {
+                        imag_check(i) = 2;
+                    }
+                }
+            }
+        }
+    
+        cx_mat AA_res = zeros<cx_mat>(Ns, N+1);
+    
+        // wypelnienie lewej strony macierzy AA_res
+        for ( int m = 0; m < N; m++ )
+        {
+            if ( imag_check(m) == 0 )
+            {
+                AA_res.col(m) = 1 / ( *s - poles(m));
+            }
+            else if ( imag_check(m) == 1 )
+            {
+                AA_res.col(m) = 1 / ( *s - poles(m)) + 1 / ( *s - conj(poles(m)));
+                AA_res.col(m+1) = 1.0i / ( *s - poles(m)) - 1.0i / ( *s - conj(poles(m)));
+            }
+        }
+    
+        AA_res.col(N) = ones<cx_mat>(1, Ns).st();
+    
+        mat AA_res_real = join_vert( real(AA_res), imag(AA_res) );
+    
+        // obliczenie metoda najmniejszych kwadratow residuów szukanej funkcji
+        x = inv( (AA_res_real.st() * AA_res_real ) ) * AA_res_real.st() * f_lsp_real;
+    
+        // zapis residuów w postaci zespolonej (jesli takie istnieja)
+   //     wynik.res = zeros<cx_mat>(1, N);
+        
+        m = 0;
+        for ( int i = 0; i < N ; i++ )
+        {
+            if ( imag_check(i) == 0 )
+            {
+     //           wynik.res(m) = x(i);
+            }
+            else if ( imag_check(i) == 1 )
+            {
+     //           wynik.res(m) = x(i) + 1i * x(i+1);
+       //         wynik.res(m+1) = conj(wynik.res(m));
+            }
+            m++;
+        }
+    
+        //wynik.h = x(x.n_elem - 1);
+        //wynik.poles = poles;
+    }
+} 
+
+
 SER my_vf_all_splitting(const cx_mat& f, const cx_vec& s, cx_vec poles)
 {
     SER wynik;
     int Nc = f.n_rows;
+    int N = poles.n_elem;
+    int Ns = s.n_elem;
+
+    // prealokacja
+    wynik.res = zeros<cx_mat>(Nc, N);
+    wynik.poles = zeros<cx_vec>(Nc, N);
+    wynik.h = zeros<mat>(Nc,1);
+    wynik.err = 0.0;
 
     for (int i = 0; i < Nc; i++ )
     {
        //wywolanie vf
     }
+
+    // obliczanie bledu metody najmniejszych kwadratow dla kazdego z portow
+    cx_mat f_check = zeros<cx_mat>(Nc, Ns);
+    for ( int m = 0; m < Nc; m++ )
+    {
+        for ( int i = 0; i < Ns; i++ )
+        {
+            for ( int j = 0; j < N; j++ )
+            {
+                f_check(m, i) = f_check(m, i) + wynik.res(m, j) / ( s(i) - wynik.poles(j));
+            }
+            f_check(m, i) = f_check(m, i) + wynik.h(m, 0);
+        } 
+    }
+     
+    cx_mat diff = f - f_check;
+
+    wynik.err = sqrt( accu ( accu( pow(abs(diff), 2) ) ) );
 
     return wynik;
 }
--- my_vectfit.h
+++ my_vectfit.h
@@ -55,14 +55,14 @@ class vf_all
 {
     const cx_mat *f;
     const cx_vec *s;
-    const cx_vec *poles;
+    cx_vec poles1;
     SER *wynik;
 
 public:
-    vf_all( const cx_mat *f_in, const cx_vec *s_in, const cx_vec *poles_in, SER *wynik_in )
+    vf_all( const cx_mat *f_in, const cx_vec *s_in, cx_vec poles_in, SER *wynik_in )
          : f(f_in),
            s(s_in),
-           poles(poles_in),
+           poles1(poles_in),
            wynik(wynik_in)
          {}
 
