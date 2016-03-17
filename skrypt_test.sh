016ede8378620d7c09d3e42753bf64a23db6794b
Piotr Hajdukiewicz <email@email.com>
2016-03-17 13:12:05 +0100

    Inicjalny komit w brnachu all splitting single thread

--- skrypt_test.sh
+++ skrypt_test.sh
@@ -4,9 +4,9 @@ poles_N=( 50 30 20 64 26 22 10 28 10 12 10 6 12 30 20 22 20 11 11 15 12 6 4 60);
 ports_Nc=( 3136 6889 60025 1156 2304 8464 1600 2116 1156 27889 1681 25600 1936 576 1225 484 484 784 625 529 1600 19600 5184 10201 );
 samples_Ns=( 1001 1228 197 570 690 71 1001 71 1000 27 572 1010 1010 10010 1450 2010 2010 5010 572 572 400 100 110 999 );
 
-:>stats_cpp_all_split.txt
+:>stats_cpp_all_split_single_thread.txt
 
-echo "N;Nc;Ns;iters;RMS_err;time_consumed" > stats_cpp_all_split.txt
+echo "N;Nc;Ns;iters;RMS_err;time_consumed" > stats_cpp_all_split_single_thread.txt
 
 for i in {0..23}
 do
