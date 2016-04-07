c501bd1d992aa02a887481ef0f91b4eff395b04c
piohaj <piotrek.hajdukiewicz@gmail.com>
2016-04-07 16:13:59 +0200

    kosmetyka

--- my_vectfit.h
+++ my_vectfit.h
@@ -62,7 +62,8 @@ class vf_column
     SER *wynik;
 
 public:
-    vf_column( const cx_mat *f_in, const cx_vec *s_in, cx_mat *poles_in, SER *wynik_in )
+    vf_column( const cx_mat *f_in, const cx_vec *s_in, cx_mat *poles_in,
+               SER *wynik_in )
          : f(f_in),
            s(s_in),
            poles1(poles_in),
